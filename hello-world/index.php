880b8c079afed35a5585f2849bab9897df2e369d
deniscordero <deniscordero1987@gmail.com>
2018-04-03 22:09:13 -0600

    cursoPHP

---
+++ hello-world/index.php
@@ -0,0 +1,12 @@
+<!DOCTYPE html>
+<html lang="span" dir="ltr">
+  <head>
+    <meta charset="utf-8">
+    <title>My first script</title>
+  </head>
+  <body>
+  <?php
+    echo "Hello world";
+  ?>
+  </body>
+</html>
